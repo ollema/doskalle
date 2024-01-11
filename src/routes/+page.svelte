522a306858a72a05ed4e67e2e28c1e67aa6f444d
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-01-11 19:25:13 +0100

    logo v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Logo from './Logo.svelte';
+</script>
+
+<div class="container">
+	<div>
+		<Logo />
+	</div>
+</div>
+
+<style>
+	.container {
+		margin: auto;
+		width: 200px;
+		aspect-ratio: 1 / 1;
+	}
+</style>
