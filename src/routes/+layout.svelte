b5c879c34a5be4ebe702dd5846c74c3a8cd25802
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-01-15 08:35:40 +0100

    more logos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
 	import '../app.pcss';
+	import '@fontsource/unifrakturcook';
 </script>
 
-<slot />
+<nav class="flex w-full items-center justify-center">
+	<ul class="flex items-center gap-8 p-4 text-xl">
+		<li class="hover:text-red-700 hover:underline">
+			<a href="/logos">all logos</a>
+		</li>
+		<li class="hover:text-red-700 hover:underline">
+			<a href="/logos/standard">with album art</a>
+		</li>
+	</ul>
+</nav>
+
+<div class="container">
+	<slot />
+</div>
--- src/routes/logos/+page.svelte
+++ src/routes/logos/+page.svelte
@@ -1,20 +1,70 @@
 <script lang="ts">
-	import logo_basic from './logo_basic.svg?raw';
-	import Standard from './Standard.svelte';
-	import StandardBoxed from './StandardBoxed.svelte';
+	import logo_basic_1 from './logo_basic_1.svg?raw';
+	import logo_basic_2 from './logo_basic_2.svg?raw';
+	import logo_filled_1 from './logo_filled_1.svg?raw';
+	import logo_filled_2 from './logo_filled_2.svg?raw';
+	import logo_filled_3 from './logo_filled_3.svg?raw';
+	import logo_boxy_1 from './logo_boxy_1.svg?raw';
+	import logo_boxy_2 from './logo_boxy_2.svg?raw';
+	import logo_boxy_3 from './logo_boxy_3.svg?raw';
+	import logo_basic_1_border from './logo_basic_1_border.svg?raw';
+	import logo_boxy_1_border from './logo_boxy_1_border.svg?raw';
+	import logo_boxy_2_border from './logo_boxy_2_border.svg?raw';
+	import logo_boxy_3_border from './logo_boxy_3_border.svg?raw';
 </script>
 
-<div class="flex gap-8 p-4 items-center">
-	<div class="w-[160px]">
-		{@html logo_basic}
+<div class="w-full text-center text-2xl">without border</div>
+<div class="flex w-full items-center justify-center gap-8 p-4">
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">basic 1</div>
+		{@html logo_basic_1}
 	</div>
-	<div>
-		<StandardBoxed />
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">basic 2</div>
+		{@html logo_basic_2}
 	</div>
-	<div>
-		<Standard filler={true} />
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">filled 1</div>
+		{@html logo_filled_1}
 	</div>
-	<div>
-		<StandardBoxed filler={true} />
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">filled 2</div>
+		{@html logo_filled_2}
+	</div>
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">filled 3</div>
+		{@html logo_filled_3}
+	</div>
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">boxy 1</div>
+		{@html logo_boxy_1}
+	</div>
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">boxy 2</div>
+		{@html logo_boxy_2}
+	</div>
+	<div class="flex w-[120px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">boxy 3</div>
+		{@html logo_boxy_3}
+	</div>
+</div>
+
+<div class="w-full pt-4 text-center text-2xl">with border</div>
+<div class="flex w-full items-center justify-center gap-8 p-4">
+	<!-- <div class="flex w-[150px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">basic 1</div>
+		{@html logo_basic_1_border}
+	</div> -->
+	<div class="flex w-[150px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">boxy 1</div>
+		{@html logo_boxy_1_border}
+	</div>
+	<div class="flex w-[150px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">boxy 2</div>
+		{@html logo_boxy_2_border}
+	</div>
+	<div class="flex w-[150px] flex-col items-center gap-4 hover:text-red-700">
+		<div class="text-xl">boxy 3</div>
+		{@html logo_boxy_3_border}
 	</div>
 </div>
